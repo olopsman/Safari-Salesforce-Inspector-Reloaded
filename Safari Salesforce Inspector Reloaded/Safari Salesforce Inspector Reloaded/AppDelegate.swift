27bb3d04ea629eade49038d6a016685e7d34af2d
Paulo Orquillo <olopsman@users.noreply.github.com>
2023-12-04 13:17:42 +1300

    Initial conversion

---
+++ Safari Salesforce Inspector Reloaded/Safari Salesforce Inspector Reloaded/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Safari Salesforce Inspector Reloaded
+//
+//  Created by Paulo Orquillo on 4/12/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
